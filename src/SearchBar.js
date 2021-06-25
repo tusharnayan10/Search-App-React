a384f0a66c13bea3dea971bcfd78813b3458a227
Tushar Nayan <tusharnayan2610@gmail.com>
2021-06-25 17:04:12 +0530

    first commit

---
+++ src/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+
+    state = { term: '' };
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={(event) => this.onFormSubmit(event)} className="ui form">
+                    <div className="field">
+                        <label>Search Image</label>
+                        <input type="text" name="search"
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value})} placeholder="Search Image" />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from '../SearchBar';
+
+function App() {
+  return (
+    <div className="App ui container" style={{marginTop: '12px'}}>
+      <SearchBar />    
+    </div>
+  );
+}
+
+export default App;
